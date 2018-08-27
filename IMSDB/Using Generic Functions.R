091d47ad71c09f0c47069401c289e905fd873a7e
don-E Merson <dmerson@email.arizona.edu>
2018-08-26 21:52:52 -0700

    REveanant/

--- IMSDB/Using Generic Functions.R
+++ IMSDB/Using Generic Functions.R
@@ -1,10 +1,12 @@
 source("IMSDB/Better Generic Function.R")
 #script_lines <- read_imsdb_file_into_lines("Screenplays/Beauty and the Beast Script at IMSDb..html")
 #script_lines <- read_imsdb_file_into_lines("Screenplays/Guardians of the Galaxy Vol 2 Script at IMSDb..html")
-script_lines <- read_imsdb_file_into_lines("Screenplays/Zootopia Script at IMSDb..html")
+#script_lines <- read_imsdb_file_into_lines("Screenplays/Zootopia Script at IMSDb..html")
+script_lines <- read_imsdb_file_into_lines("Screenplays/Revenant, The Script at IMSDb..html")
 #script_df <-create_script_df(script_lines,"Beauty and The Beast")
 #script_df <-create_script_df(script_lines,"Gaurdians of the Galaxy")
-script_df <-create_script_df(script_lines,"Zootopia")
+#script_df <-create_script_df(script_lines,"Zootopia")
+script_df <-create_script_df(script_lines,"Revenant")
 View(script_df)
 View(sqldf("select current_title,current_character, current_dialogue 
            from script_df 
